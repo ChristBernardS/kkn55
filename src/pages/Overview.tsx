9ec1bac8fe8db7039d2fb26297b332d4a31bb7ef
Christopher B. S. <117885718+ChristBernardS@users.noreply.github.com>
2025-07-24 12:34:11 +0700

    Update Overview.tsx

--- src/pages/Overview.tsx
+++ src/pages/Overview.tsx
@@ -6,6 +6,7 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 
+// Membuat instance ikon kustom yang menyerupai penanda Google Maps
 const customMarkerIcon = new L.Icon({
   iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
@@ -149,11 +150,11 @@ export default function Overview() {
               <MapContainer center={position} zoom={14} scrollWheelZoom={false} style={{ height: '100%', width: '100%' }}>
                 <TileLayer
                   attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
                 />
                 <Marker position={position} icon={customMarkerIcon}>
                   <Popup>
-                    <b>Lokasi Proyek KKN</b> <br /> Desa Gubugklakah, Poncokusumo, Malang.
+                    <b>Lokasi KKN</b> <br /> Desa Dersono, Dusun Sumur, Kabupaten Pacitan.
                   </Popup>
                 </Marker>
               </MapContainer>
